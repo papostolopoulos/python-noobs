99e42a666e837937c7fe6b65b96d361e719b96a8
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-10-29 10:37:40 -0700

    commit

---
+++ test.week4/ex27.py
@@ -0,0 +1,78 @@
+# Exercise 27: Memorizing Logic
+
+print "---EXERCISE 27: MEMORIZING LOGIC---"
+
+
+print """
+----------
+NOT
+----------
+"""
+print "not False:", not False
+print "not True:", not True
+
+
+print """
+----------
+OR
+----------
+"""
+print "True or False:", True or False
+print "True or True:", True or True
+print "False or True:", False or True
+print "False or False:", False or False
+
+
+print """
+----------
+AND
+----------
+"""
+print "True and False:", True and False
+print "True and True:", True and True
+print "False and True:", False and True
+print "False and False:", False and False
+
+
+print """
+----------
+NOT OR
+----------
+"""
+print "not(True or False:)", not(True or False)
+print "not(True or True:)", not(True or True)
+print "not(False or True:)", not(False or True)
+print "not(False or False:)", not(False or False)
+
+
+print """
+----------
+NOT AND
+----------
+"""
+print "not(True and False:)", not(True and False)
+print "not(True and True:)", not(True and True)
+print "not(False and True:)", not(False and True)
+print "not(False and False:)", not(False and False)
+
+
+print """
+----------
+!=
+----------
+"""
+print "1 != 0:", 1 != 0
+print "1 != 1:", 1 != 1
+print "0 != 1:", 0 != 1
+print "0 != 0:", 0 != 0
+
+
+print """
+----------
+==
+----------
+"""
+print "1 == 0:", 1 == 0
+print "1 == 1:", 1 == 1
+print "0 == 1:", 0 == 1
+print "0 == 0:", 0 == 0
---
+++ test.week4/ex28.py
@@ -0,0 +1,80 @@
+# Exercise 28: Boolean Practice
+print "---EXERCISE 28: BOOLEAN PRACTICE---"
+
+
+
+boolean_list = [["True and True", True and True],
+["False and True", False and True],
+['1==1 and 2==1', 1==1 and 2==1],
+['"test" == "test"', "test" == "test"],
+['1==1 or 2!=1', 1==1 or 2!=1],
+['True and 1==1', True and 1==1],
+['False and 0 != 0', False and 0 != 0],
+['True or 1==1', True or 1==1],
+['"test" == "testing"', "test" == "testing"],
+['1!=0 and 2==1', 1!=0 and 2==1],
+['"test" != "testing"', "test" != "testing"],
+[],
+[],
+[],
+[],
+[],
+[],
+[],
+[],
+[]
+
+'"test" == 1',
+'not (True and False)',
+'not (1==1 and 0!=1)',
+'not (10 == 1 or 1000 == 1000)',
+'not (1!=10 or 3==4)',
+'not ("testing" == "testing" and "Zed" == "Cool Guy")',
+'1==1 and not ("testing"==1or1==0)',
+'"chunky" == "bacon" and not (3 == 4 or 3 == 3)',
+'3 == 3 and not ("testing" == "testing" or "Python" == "Fun")']
+
+def boolean_test(compare_list):
+    print "Hello there. Let's do some boolean practice. This test has %s questions. Good luck!" % len(compare_list)
+    print ""
+    final_score = 0
+
+    for x in range(0, len(compare_list)):
+        question = raw_input("Question " + str(x + 1) + ": What is the value for " + str(compare_list[x]) + "? ")
+        user_answer = question.lower().capitalize()
+        print type(bool(user_answer)), type(bool(compare_list[x]))
+        if(user_answer == str(compare_list[x])):
+            final_score += 1
+            print "That is correct! " + str(compare_list[x]) + " has a boolean value of " + str(compare_list[x]) + "\n"
+        else:
+            print "Sorry, that is incorrect. " + str(compare_list[x]) + " has a boolean value of " + str(bool(compare_list[x]))
+
+    print "Out of the " + str(len(compare_list)) + " questions, you got " + str(final_score) + " correct! \n Thanks for playing!"
+
+
+boolean_test(boolean_list)
+
+# x = raw_input("")
+# print x
+# print type(bool(x))
+
+# 1. True and True
+# 2. False and True
+# 3. 1==1and2==1
+# 4. "test" == "test"
+# 5. 1==1or2!=1
+# 6. Trueand1==1
+# 7. False and 0 != 0
+# 8. Trueor1==1
+# 9. "test" == "testing"
+# 10. 1!=0and2==1
+# 11. "test" != "testing"
+# 12. "test" == 1
+# 13. not (True and False)
+# 14. not(1==1and0!=1)
+# 15. not (10 == 1 or 1000 == 1000)
+# 16. not(1!=10or3==4)
+# 17. not ("testing" == "testing" and "Zed" == "Cool Guy")
+# 18. 1==1andnot("testing"==1or1==0)
+# 19. "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+# 20. 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
