69db64aac92147b04faf97888d14a1c840708f18
Paraskevas Apostolopoulos <papost@fb.com>
2017-11-04 22:14:14 -0700

    commit

---
+++ test.week4/test1-week4.py
@@ -0,0 +1,5 @@
+Test - Print line numbers (II) #
+Revisit your test from Week 3 and do the same thing for all the lines in the input file. Edit the same file test3.py to make your changes.
+
+Test - count words #
+Write function that returns the number of words in a string. Save it in a file called test4.py and push it with sourcetree.
