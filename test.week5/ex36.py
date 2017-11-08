fbe4343599f36e242e52e2253ce8d62c017add9d
Paraskevas Apostolopoulos <papost@fb.com>
2017-11-07 18:26:36 -0800

    commit

---
+++ test.week5/ex36.py
@@ -0,0 +1,18 @@
+# Exercise 36: Designing and Debugging
+print "---EXERCISE 36: DESIGNING AND DEBUGGING"
+
+def game_start():
+    print "This is the beggining of the game"
+
+def lions_enclosure():
+    print "This is the Lions cage"
+
+def tigers_enclosure():
+    print "This is the tigers enclosure"
+
+def sharks_pool():
+    print "This is the sharks pool"
+
+def death(action):
+    print action, "You are an angel in the sky"
+    exit(0)
---
+++ test.week5/test5.py
@@ -0,0 +1,21 @@
+numbers = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+}
+
+
+def phone_number_to_english():
+    print "Hello, This program can help you convert a phone number in words. Please enter your number"
+    user_entry = raw_input(": ")
+
+    
+
+print numbers
